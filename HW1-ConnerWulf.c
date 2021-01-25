9bec9e6616822e3db70791ed8eba426a308fa10c
Conner <connerwulf@usf.edu>
2021-01-24 19:02:30 -0500

    error

--- HW1-ConnerWulf.c
+++ HW1-ConnerWulf.c
@@ -1,6 +1,7 @@
-int int main(int argc, char const *argv[]) {
-  int child1 = argv[1];
-  int child2 = argv[1] + 3;
+#include<stdio.h>
+int main(int argc, char const *argv[]) {
+  int child1 = (int)argv[1];
+  int child2 = (int)argv[1] + 3;
   printf("%d\n",child2);
   return 0;
 }
