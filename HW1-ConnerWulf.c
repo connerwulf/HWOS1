096be111b79a25fd3154478b23b5ae4bfbc851da
Conner <connerwulf@usf.edu>
2021-01-24 19:23:39 -0500

    axdaerf

--- HW1-ConnerWulf.c
+++ HW1-ConnerWulf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(int argc, char const *argv[]) {
 
-  string child1 = *argv[1];
+  String child1 = *argv[1];
   //int child2 = argv[1] + 3;
   printf("%s\n",child1);
   return 0;
