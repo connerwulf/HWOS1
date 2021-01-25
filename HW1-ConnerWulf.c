de228c946a2a557f61044d4970670e32a0de1ed9
Conner <connerwulf@usf.edu>
2021-01-24 20:43:55 -0500

    Code Formatting

--- HW1-ConnerWulf.c
+++ HW1-ConnerWulf.c
@@ -6,27 +6,29 @@
 
 int main(int argc, char const *argv[]) {
 
+  //Intialization
   const int SIZE = 2;
   int value = atoi(argv[1]);
+
+  //Bound Check
   if(value < 0 || value > 40)
   {
     printf("Value must be between 0 and 40\n");
     return 0;
   }
+
+  //Main Code - Where Parent creates Childern
   for (int i = 1; i <= SIZE; i++)
   {
+    //Childern Created
       pid_t pid = fork();
 
-      if(pid == -1)
+      //Start of Child process
+      if(pid == 0)
       {
-        printf("\nFork failed");
-        return 0;
-      }
-      else if(pid == 0)
-      {
-        pid_t pid_c = getpid();
         printf("\tFrom Child %d init n=%d, ", i, value);
 
+        //Collatz algorithm
         while(value != 1)
         {
           if(value % 2 != 0)
@@ -47,10 +49,13 @@ int main(int argc, char const *argv[]) {
         value += 3;
       }
   }
+
+  //Parent process waits for each child.
   for(int j = 0; j < SIZE; j++)
   {
     wait(NULL);
   }
+  //Parent process Competed
   printf("\tChildern Complete\n");
   return 0;
 }
