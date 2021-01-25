0c5c87134cb32044c0c047450c75bbf23c631024
Conner <connerwulf@usf.edu>
2021-01-24 20:27:37 -0500

    newline

--- HW1-ConnerWulf.c
+++ HW1-ConnerWulf.c
@@ -34,7 +34,7 @@ int main(int argc, char const *argv[]) {
           }
           printf("From Child %d n=%d, ", i, value);
         }
-
+        printf("\n");
         return 0;
       }
       else
