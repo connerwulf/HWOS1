c19ddad7d69bd66122883b00829775bb55d91f14
Conner <connerwulf@usf.edu>
2021-01-24 19:29:42 -0500

    caps

--- HW1-ConnerWulf.c
+++ HW1-ConnerWulf.c
@@ -3,7 +3,7 @@
 int main(int argc, char const *argv[]) {
 
   int child1 = atoi(argv[1]);
-  int child2 = child1 + 3
+  int child2 = child1 + 3;
   //int child2 = argv[1] + 3;
   printf("%d, %d\n",child1, child2);
   return 0;
