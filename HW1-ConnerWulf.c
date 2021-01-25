1ccbc5c13a4040bfaf963da981881962776abe4a
Conner <connerwulf@usf.edu>
2021-01-24 19:16:43 -0500

    please work

--- HW1-ConnerWulf.c
+++ HW1-ConnerWulf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(int argc, char const *argv[]) {
   int child1 = argv[1];
-  int child2 = argv[1] + 3;
-  printf("%d\n",child2);
+  //int child2 = argv[1] + 3;
+  printf("%d\n",child1);
   return 0;
 }
