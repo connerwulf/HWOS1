3475b823f280337b936891d9ccc0014a661c73e8
Conner <connerwulf@usf.edu>
2021-01-24 20:19:22 -0500

    Missing return statement

--- HW1-ConnerWulf.c
+++ HW1-ConnerWulf.c
@@ -8,7 +8,7 @@ int main(int argc, char const *argv[]) {
 
   const int SIZE = 2;
   int value = atoi(argv[1]);
-  printf("%d\n", value);
+
   for (int i = 1; i <= SIZE; i++)
   {
       pid_t pid = fork();
@@ -35,7 +35,7 @@ int main(int argc, char const *argv[]) {
           printf("From Child %d n=%d, ", i, value);
         }
 
-
+        return 0;
       }
       else
       {
