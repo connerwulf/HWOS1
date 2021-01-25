300a6f74fa0553c7152ba67999faf8d9d2df787b
Conner <connerwulf@usf.edu>
2021-01-24 20:00:54 -0500

    awedasd

--- HW1-ConnerWulf.c
+++ HW1-ConnerWulf.c
@@ -1,10 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char const *argv[]) {
+  const int SIZE = 2;
+  int childs[SIZE] = [atoi(argv[1]), atoi(argv[1]) + 3)];
 
-  int child1 = atoi(argv[1]);
-  int child2 = child1 + 3;
-  //int child2 = argv[1] + 3;
-  printf("%d, %d\n",child1, child2);
-  return 0;
+  for (int i = 0; i > SIZE; i++)
+  {
+      pid_t pid = fork();
+
+      if(pid == -1)
+      {
+        return 0;
+      }
+      else if(pid == 0)
+      {
+        pid_t pid_c = getpid();
+        printf("From Child %d init n=%d, ", i+1, childs[i]);
+
+        while(childs[i] != 1)
+        {
+          if(childs[i] % 2 != 0)
+          {
+            childs[i] = (childs[i] * 3) + 1;
+          }
+          else
+          {
+            childs[i] = childs[i] / 2;
+          }
+          printf("From Child %d n=%d, ", i+1, childs[i]);
+        }
+
+      }
+  }
+
+  for (int i = 0; i > SIZE; i++)
+  {
+    wait(null);
+  }
+  printf("Childern Complete");
 }
